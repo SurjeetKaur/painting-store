018eec4ff7f1b7d2a68dda714eb9cb873b154375
Surjeet Kaur <surjeetkaur961@gmail.com>
2026-04-29 15:23:42 -0400

    updated explore collection

--- src/components/ExploreCollections.js
+++ src/components/ExploreCollections.js
@@ -3,6 +3,7 @@ import { MOCK_DATA } from "../constants/mockData.js";
 import WavyDivider from "./WavyDivider.js";
 import{useNavigate} from "react-router-dom";
 import React, { useState, useEffect } from "react";
+import Shimmer from "./Shimmer.js";
 
 function ExploreCollections() {
   const navigate=useNavigate();
@@ -14,11 +15,32 @@ function ExploreCollections() {
     }, []);
   
     if (!collectionData) {
-      // Shimmer loader 
+      // Shimmer loader
+      const shimmerCount = MOCK_DATA.collections.length;  
       return(
-        <p>Loading...</p>
-      )  
-    }
+              <section className="relative py-12 px-6 md:px-20 bg-gray-100">
+                <h2 className="text-3xl font-bold text-center mb-8">
+                  Explore Collections
+                </h2>
+
+                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-8 mb-10">
+                  {Array(shimmerCount)
+                    .fill()
+                    .map((_, index) => (
+                      <div
+                        key={index}
+                        className="bg-white rounded-2xl shadow p-6 text-center"
+                      >
+                        <Shimmer width="100%" height="14rem" className="mb-4" />
+                        <Shimmer width="70%" height="1.5rem" className="mx-auto" />
+                      </div>
+                    ))}
+                </div>
+
+                <WavyDivider color="#F8E0C8" />
+              </section>
+            );
+          }
   return (
     <section className="relative py-12 px-6 md:px-20 bg-gray-100">
       <h2 className="text-3xl font-bold text-center mb-8">Explore Collections</h2>
